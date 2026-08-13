a44a22f4c3030f3564146bda65261605f1d53490
Sergio Conde <124182273+sergio-conde@users.noreply.github.com>
2026-08-13 13:39:43 +0200

    Create getEntry.m

---
+++ data_io/getEntry.m
@@ -0,0 +1,135 @@
+function [selEntry,entryLine] = getEntry(entryList,varargin)
+
+% [selEntry,entryLine] = getEntry(entryList,varargin)
+%
+% pick_files function lists files from the ref_files (output of the proj_organigram function)
+%
+%   Inputs:
+%     ref_files: output of the proj_organigram function                     [struct]
+%                Note: However, this function shuould work to select
+%                specific lines of any structure.
+%     field_name
+%     field_value: selecting criteria for field_name. 
+%          
+%   Outputs:
+%     out_files: Output struct containing the same fiels as ref_files
+%       name: file name
+%       folder: file folder
+%       file_path: full file path
+%       + one field per level following the name con cfg.level_name input
+%
+% Sergio Conde-Ocazionez, August 2024. 
+% Neuromodulation & Behavior Laboratory
+% Netherlands Institute for Neuroscience.
+
+% check minimum number of input arguments
+if nargin < 2
+    error('Not enough input arguments')
+end
+
+% check input list
+if istable(entryList)
+    structFlag = false;
+elseif isstruct(entryList)
+    structFlag = true;
+    entryList = struct2table(entryList);
+else
+    error('Input data should be a table or indexed struct')
+end
+
+% entryList features
+nEntries = height(entryList);
+numEntry = varfun(@isnumeric,trl_list,'OutputFormat','uniform');
+
+ogRequest = varargin;
+if istruct(ogRequest{1})
+    requestConfig = ogRequest;
+else
+    requestConfig = setRequest(ogRequest);
+end
+
+% fields/variables from the input structure/table
+validFields = entryList.Properties.VariableNames;
+
+% select valid fields from the request, check contrasts
+requestConfig = checkRequest(requestConfig,validFields);
+
+% % set the contrast information
+% requestConfig = checkContrast(requestConfig);
+
+% select data from each variable
+requestVar = setdiff(fieldnames(requestConfig),'contrast');
+
+nVars = length(requestVar);
+varFlags = true(nEntries,nVars);
+for ivar = 1:nVars
+    requestLabel = requestVar{ivar};
+    requestValue = requestConfig.(requestLabel);
+    varData = entryList.(requestLabel);
+    if numEntry(ivar)
+        switch requestConfig.contrast.(requestLabel)
+            case 'lower'
+                varFlags(:,ivar) = varData < requestValue;
+            case 'higher'
+                varFlags(:,ivar) = varData > requestValue;
+            case 'equal'
+                varFlags(:,ivar) = varData == requestValue;
+            case 'range'
+                varFlags(:,ivar) = varData > requestValue(1) & ...
+                    varData < requestValue(2);
+        end
+    else
+        varFlags(:,ivar) = ismember(varData,requestValue);
+    end
+end
+pickFlags = all(varFlags,2);
+entryLine = find(pickFlags);
+
+selEntry = entryList(pickFlags,:);
+if structFlag
+    selEntry = table2struct(selEntry);
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function requestConfig = setRequest(ogRequest)
+
+requestFields = ogRequest(1:2:end);
+requestValues = ogRequest(2:2:end);
+
+if all(cellfun(@ischar,requestFields))
+    for irequest = 1:length(requestFields)
+        requestConfig.(requestFields{irequest}) = requestValues{irequest};
+        requestConfig.contrast.(requestFields{irequest}) = 'equal';
+    end
+else
+    %errormsg
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function requestConfig = checkRequest(requestConfig)
+
+% checkContrast checks and sets the default contrast value ('equal') for 
+% all requested fields
+
+requestFields = setdiff(fieldnames(requestConfig),'contrast');
+invalidFields = setdiff(requestFields,validFields);
+if ~isempty(invalidFields)
+    warning(['The following requested fields are not in the entry list'...
+        ' and were ignored: \n%s\n'],...
+        strjoin(invalidFields, '\n'));
+    requestFields = setdiff(requestFields,invalidFields);
+end
+
+% set the default empty contrast definition if needed
+if ~isfield(requestConfig,'contrast')
+    requestConfig.contrast = [];
+end
+
+for irequest = 1:length(requestFields)
+    if ~isfield(requestConfig.contrast,requestFields{irequest})
+        requestConfig.contrast.(requestFields{irequest}) = 'equal';
+    end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
