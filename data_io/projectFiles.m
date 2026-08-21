201d3ad6012fbe319c3d78b2ef716583dbc7799f
Sergio Conde <124182273+sergio-conde@users.noreply.github.com>
2026-08-21 15:42:07 +0200

    style update

--- data_io/projectFiles.m
+++ data_io/projectFiles.m
@@ -7,17 +7,17 @@ function fileList = projectFiles(cfg)
 %
 %   Inputs:
 %     cfg: structure containing the following fields
-%       main_path: main project folder path                                            [string]
-%       level_name: name associated with each level of the organigram.                 [cell array of strings]
-%       folder_coding: string codification of the folders associated to each level.    [cell array of strings]
-%       file_str: string codification of the required files. If empty, all files on    [string]
+%       mainPath: main project folder path                                            [string]
+%       levelName: name associated with each level of the organigram.                 [cell array of strings]
+%       folderCode: string codification of the folders associated to each level.    [cell array of strings]
+%       fileCode: string codification of the required files. If empty, all files on    [string]
 %                 each folder will be listed.                                          
 %   Outputs:
 %     out_files: Output struct containing the following fields
 %       name: file name
 %       folder: file folder
 %       file_path: full file path
-%       + one field per level following the name con cfg.level_name input
+%       + one field per level following the name con cfg.levelName input
 %
 % v1.0: April 2023
 % v2.0: August 2026
@@ -28,36 +28,36 @@ function fileList = projectFiles(cfg)
 
 
 % defining the search path %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-catPath = cfg.main_path;                                       % main project folder 
-for ilevel = 1:length(cfg.level_name)
-    catPath = cat(2,catPath,['\' cfg.folder_coding{ilevel}]); % concatenate all levels 
+catPath = cfg.mainPath;                                       % main project folder 
+for ilevel = 1:length(cfg.levelName)
+    catPath = cat(2,catPath,['\' cfg.folderCode{ilevel}]); % concatenate all levels 
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % if there is a string code for the file names %%%%%%%%%%%%%%%%%%%%%%%%%
-if ~isempty(cfg.file_str)
-    catPath = cat(2,catPath,['\' cfg.file_str]); % add to search path
+if ~isempty(cfg.fileCode)
+    catPath = cat(2,catPath,['\' cfg.fileCode]); % add to search path
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-all_files = dir(catPath); % all files matching the configuration 
+allFiles = dir(catPath); % all files matching the configuration 
 
-main_length = length(cfg.main_path);                                    % main project folder str length 
+mainLength = length(cfg.mainPath);                                    % main project folder str length 
 fileList = struct([]);                                                 % output structure initialization
 
-for ifile = 1:length(all_files)
+for ifile = 1:length(allFiles)
     
     % create string references to extract level names %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    bslash = strfind(all_files(ifile).folder,'\');                                  % backslash positions 
-    ref_pos = [bslash(bslash >= main_length) length(all_files(ifile).folder) + 1];  % add final reference
+    bslash = strfind(allFiles(ifile).folder,'\');                                  % backslash positions 
+    refPos = [bslash(bslash >= mainLength) length(allFiles(ifile).folder) + 1];  % add final reference
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     % fill output structure %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    for ilevel = 1:length(cfg.folder_coding)
-        fileList(ifile).name = all_files(ifile).name;                                      % file name
-        fileList(ifile).folder = all_files(ifile).folder;                                  % file folder
-        fileList(ifile).file_path = [all_files(ifile).folder '\' fileList(ifile).name];   % full file path
-        fileList(ifile).(cfg.level_name{ilevel}) = ...
-            all_files(ifile).folder(ref_pos(ilevel) + 1 : ref_pos(ilevel + 1) - 1);         % level name
+    for ilevel = 1:length(cfg.folderCode)
+        fileList(ifile).name = allFiles(ifile).name;                                      % file name
+        fileList(ifile).folder = allFiles(ifile).folder;                                  % file folder
+        fileList(ifile).file_path = [allFiles(ifile).folder '\' fileList(ifile).name];   % full file path
+        fileList(ifile).(cfg.levelName{ilevel}) = ...
+            allFiles(ifile).folder(refPos(ilevel) + 1 : refPos(ilevel + 1) - 1);         % level name
     end
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 end
